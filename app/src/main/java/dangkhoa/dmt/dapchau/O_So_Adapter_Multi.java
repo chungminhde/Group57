7992df79c2dad8350fc6a08bc7f0d9affcbe5898
doductrung24181999 <48834621+doductrung24181999@users.noreply.github.com>
2019-05-22 20:20:33 +0700

    O_So_Adapter_Multi.java

---
+++ app/src/main/java/dangkhoa/dmt/dapchau/O_So_Adapter_Multi.java
@@ -0,0 +1,60 @@
+package dangkhoa.dmt.dapchau;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class O_So_Adapter_Multi extends ArrayAdapter<Integer> {
+
+    private Context ct;
+    private ArrayList<Integer> arr;
+    private ArrayList<Integer> loai;
+
+    public O_So_Adapter_Multi(@NonNull Context context, int resource, @NonNull List<Integer> objects) {
+        super(context, resource, objects);
+        this.ct = context;
+        this.arr = new ArrayList<>(objects);
+        this.loai = new ArrayList<>(objects);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null)
+        {
+            LayoutInflater inflater = (LayoutInflater)ct.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.item_o_vuong ,null);
+        }
+        if(arr.size() > 0)
+        {
+            O_Vuong o = (O_Vuong)convertView.findViewById(R.id.txvOVuong);
+            o.setHinh(arr.get(position));
+            if(loai.get(position) == 1)
+            {
+                Animation animation = AnimationUtils.loadAnimation(ct,R.anim.anim_scale);
+                convertView.startAnimation(animation);
+            }
+            else if(loai.get(position) == 2)
+            {
+                Animation animation = AnimationUtils.loadAnimation(ct,R.anim.anim_scale_appear);
+                convertView.startAnimation(animation);
+            }
+        }
+        return convertView;
+    }
+    public void notifyDataSetChangedGame(ArrayList<Integer> a, ArrayList<Integer> b) {
+        arr = a;
+        loai = b;
+        super.notifyDataSetChanged();
+    }
+}
